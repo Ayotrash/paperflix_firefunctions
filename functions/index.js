68f199546833e0ae7da70a511611257dc1974109
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2019-11-01 19:55:13 +0700

    Added register API

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
+require('dotenv').config();
 const functions = require('firebase-functions');
+const admin     = require('firebase-admin');
+const serviceAccount = require('./paperflix-firebase-key.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const firebaseConfig = {
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://paperflix-company.firebaseio.com"
+}
+
+admin.initializeApp(firebaseConfig);
+
+const REGISTER = require('./src/register');
+
+exports.register = functions.https.onRequest(REGISTER);
